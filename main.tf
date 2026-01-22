8ae3e97ab45992fbf13bcdfe30e83179c6584dd2
Naresh Gantala <nareshgantala@gmail.com>
2026-01-22 17:30:08 +0530

    minikube setup

---
+++ main.tf
@@ -0,0 +1,72 @@
+resource "aws_vpc" "minikube_vpc" {
+  cidr_block = var.cidr_block
+    tags = {
+    Name = "minikube_vpc"
+  }
+}
+
+resource "aws_subnet" "minikube_subnet" {
+  vpc_id     = aws_vpc.minikube_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "minikube_subnet"
+  }
+}
+
+resource "aws_security_group" "minikube" {
+  vpc_id = aws_vpc.minikube_vpc.id
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "minikube_sg"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.minikube_vpc.id
+
+  tags = {
+    Name = "minikube_gw"
+  }
+}
+
+resource "aws_route_table" "minikube" {
+  vpc_id = aws_vpc.minikube_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "minikube_route"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.minikube_subnet.id
+  route_table_id = aws_route_table.minikube.id
+}
+
+resource "aws_instance" "minikube" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  subnet_id = aws_subnet.minikube_subnet.id
+  associate_public_ip_address = true
+  user_data_replace_on_change = true
+  user_data = file("user_data.sh")
+  instance_type = "t3.medium"
+  vpc_security_group_ids = [aws_security_group.minikube.id]
+  tags = {
+    Name = "minikube"
+  }
+}
---
+++ user_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euxo pipefail
+# Add Docker's official GPG key:
+apt update -y
+apt install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update -y
+
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+echo "Bootstrap completed successfully" > /home/ubuntu/bootstrap-done.txt
+chown ubuntu:ubuntu /home/ubuntu/bootstrap-done.txt
